df9c8754a2f8e1f752e6bf3ed3bf4efc1e18bd12
archna2981 <71487798+archna2981@users.noreply.github.com>
2021-08-02 14:38:54 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    text("N O F T N I U C",700,150)
+    fill("lightblue")
+    text("Hint: Used to preforme tasks !!",700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S E B",100,310)
+    fill("lightblue")
+    text("Hint: Used to store important information !!",100,330)
+}
